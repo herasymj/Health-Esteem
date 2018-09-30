98d814ae7b91202a56d49430ba573f16869cc3e8
TristanHeisler <TristanHeisler@hotmail.ca>
2018-09-30 15:29:20 -0600

    Added a second division with associated units to the database initialization process.

--- src/eIDEAS/eIDEAS/Data/DbInitializer.cs
+++ src/eIDEAS/eIDEAS/Data/DbInitializer.cs
@@ -14,8 +14,15 @@ namespace eIDEAS.Data
             }
 
             //Initialize the division
-            var division = new Division { Name = "eHealth" };
-            context.Division.Add(division);
+            var divisions = new Division[]
+            {
+                new Division { Name = "eHealth" },
+                new Division { Name = "ENSE 496ab" }
+            };
+            foreach (Division division in divisions)
+            {
+                context.Division.Add(division);
+            }
             context.SaveChanges();
 
             //Initialize the units
@@ -24,7 +31,11 @@ namespace eIDEAS.Data
                 new Unit{ DivisionID = 1, Name = "Knowledge Management"},
                 new Unit{ DivisionID = 1, Name = "Finance"},
                 new Unit{ DivisionID = 1, Name = "Continuous Improvement"},
-                new Unit{ DivisionID = 1, Name = "Service Desk"}
+                new Unit{ DivisionID = 1, Name = "Service Desk"},
+                new Unit{ DivisionID = 2, Name = "Health Esteem"},
+                new Unit{ DivisionID = 2, Name = "Tim's Bits"},
+                new Unit{ DivisionID = 2, Name = "!Cool"},
+                new Unit{ DivisionID = 2, Name = "MudsnakeFC"}
             };
 
             foreach(Unit unit in units)
