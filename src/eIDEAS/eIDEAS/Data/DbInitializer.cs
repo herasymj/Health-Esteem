d1dead1bb99d4b7fb93a0cab245f5cfa9da0cbeb
oscar666666 <1660325367@qq.com>
2018-10-04 17:33:42 -0600

    Added user initialization

--- src/eIDEAS/eIDEAS/Data/DbInitializer.cs
+++ src/eIDEAS/eIDEAS/Data/DbInitializer.cs
@@ -8,7 +8,7 @@ namespace eIDEAS.Data
         public static void Initialize(ApplicationDbContext context)
         {
             //If a division exists, the database has already been initialized
-            if(context.Division.Any())
+            if (context.Division.Any())
             {
                 return;
             }
@@ -38,10 +38,141 @@ namespace eIDEAS.Data
                 new Unit{ DivisionID = 2, Name = "MudsnakeFC"}
             };
 
-            foreach(Unit unit in units)
+            foreach (Unit unit in units)
             {
                 context.Unit.Add(unit);
             }
+            context.SaveChanges();
+
+            //Initialize the users
+            context.Users.AddRange(
+                     new ApplicationUser
+                     {
+                         UserName = "Oscar@HealthEsteem.ca",
+                         NormalizedUserName = "OSCAR@HEALTHESTEEM.CA",
+                         Email = "Oscar@HealthEsteem.ca",
+                         NormalizedEmail = "OSCAR@HEALTHESTEEM.CA",
+                         EmailConfirmed = false,
+                         PasswordHash = "AQAAAAEAACcQAAAAEE9w7IV4HKixVxUI2dL9uL7pO/b4bDlYefgEHfXY7ItrQ1Jbvia8xlxbPRDOH++Tsw==", //Abc123!
+                         SecurityStamp = "AW74NOXTMIRJLAGYJOL34XFRDUZAKCNJ",
+                         ConcurrencyStamp = "b3103776-1b4b-47e0-af12-a35245e52f4c",
+                         PhoneNumber = null,
+                         PhoneNumberConfirmed = false,
+                         TwoFactorEnabled = false,
+                         LockoutEnd = null,
+                         LockoutEnabled = true,
+                         AccessFailedCount = 0,
+                         FirstName = "Oscar",
+                         LastName = "Lou",
+                         DivisionID = 2,
+                         UnitID = 5
+                     },
+                     new ApplicationUser
+                     {
+                         UserName = "Tristan@HealthEsteem.ca",
+                         NormalizedUserName = "TRISTAN@HEALTHESTEEM.CA",
+                         Email = "Tristan@HealthEsteem.ca",
+                         NormalizedEmail = "TRISTAN@HEALTHESTEEM.CA",
+                         EmailConfirmed = false,
+                         PasswordHash = "AQAAAAEAACcQAAAAEE9w7IV4HKixVxUI2dL9uL7pO/b4bDlYefgEHfXY7ItrQ1Jbvia8xlxbPRDOH++Tsw==", //Abc123!
+                         SecurityStamp = "AW74NOXTMIRJLAGYJOL34XFRDUZAKCNJ",
+                         ConcurrencyStamp = "b3103776-1b4b-47e0-af12-a35245e52f4c",
+                         PhoneNumber = null,
+                         PhoneNumberConfirmed = false,
+                         TwoFactorEnabled = false,
+                         LockoutEnd = null,
+                         LockoutEnabled = true,
+                         AccessFailedCount = 0,
+                         FirstName = "Tristan",
+                         LastName = "Heisler",
+                         DivisionID = 2,
+                         UnitID = 5
+                     },
+                     new ApplicationUser
+                     {
+                         UserName = "Jennifer@HealthEsteem.ca",
+                         NormalizedUserName = "JENNIFER@HEALTHESTEEM.CA",
+                         Email = "Jennifer@HealthEsteem.ca",
+                         NormalizedEmail = "JENNIFER@HEALTHESTEEM.CA",
+                         EmailConfirmed = false,
+                         PasswordHash = "AQAAAAEAACcQAAAAEE9w7IV4HKixVxUI2dL9uL7pO/b4bDlYefgEHfXY7ItrQ1Jbvia8xlxbPRDOH++Tsw==", //Abc123!
+                         SecurityStamp = "AW74NOXTMIRJLAGYJOL34XFRDUZAKCNJ",
+                         ConcurrencyStamp = "b3103776-1b4b-47e0-af12-a35245e52f4c",
+                         PhoneNumber = null,
+                         PhoneNumberConfirmed = false,
+                         TwoFactorEnabled = false,
+                         LockoutEnd = null,
+                         LockoutEnabled = true,
+                         AccessFailedCount = 0,
+                         FirstName = "Jennifer",
+                         LastName = "Herasymuik",
+                         DivisionID = 2,
+                         UnitID = 5
+                     },
+                     new ApplicationUser
+                     {
+                         UserName = "Quinn@HealthEsteem.ca",
+                         NormalizedUserName = "QUINN@HEALTHESTEEM.CA",
+                         Email = "Quinn@HealthEsteem.ca",
+                         NormalizedEmail = "QUINN@HEALTHESTEEM.CA",
+                         EmailConfirmed = false,
+                         PasswordHash = "AQAAAAEAACcQAAAAEE9w7IV4HKixVxUI2dL9uL7pO/b4bDlYefgEHfXY7ItrQ1Jbvia8xlxbPRDOH++Tsw==", //Abc123!
+                         SecurityStamp = "AW74NOXTMIRJLAGYJOL34XFRDUZAKCNJ",
+                         ConcurrencyStamp = "b3103776-1b4b-47e0-af12-a35245e52f4c",
+                         PhoneNumber = null,
+                         PhoneNumberConfirmed = false,
+                         TwoFactorEnabled = false,
+                         LockoutEnd = null,
+                         LockoutEnabled = true,
+                         AccessFailedCount = 0,
+                         FirstName = "Quinn",
+                         LastName = "Bast",
+                         DivisionID = 2,
+                         UnitID = 5
+                     },
+                     new ApplicationUser
+                     {
+                         UserName = "Shawn@HealthEsteem.ca",
+                         NormalizedUserName = "SHAWN@HEALTHESTEEM.CA",
+                         Email = "Shawn@HealthEsteem.ca",
+                         NormalizedEmail = "SHAWN@HEALTHESTEEM.CA",
+                         EmailConfirmed = false,
+                         PasswordHash = "AQAAAAEAACcQAAAAEE9w7IV4HKixVxUI2dL9uL7pO/b4bDlYefgEHfXY7ItrQ1Jbvia8xlxbPRDOH++Tsw==", //Abc123!
+                         SecurityStamp = "AW74NOXTMIRJLAGYJOL34XFRDUZAKCNJ",
+                         ConcurrencyStamp = "b3103776-1b4b-47e0-af12-a35245e52f4c",
+                         PhoneNumber = null,
+                         PhoneNumberConfirmed = false,
+                         TwoFactorEnabled = false,
+                         LockoutEnd = null,
+                         LockoutEnabled = true,
+                         AccessFailedCount = 0,
+                         FirstName = "Shawn",
+                         LastName = "Clake",
+                         DivisionID = 2,
+                         UnitID = 5
+                     },
+                     new ApplicationUser
+                     {
+                         UserName = "Wilson@HealthEsteem.ca",
+                         NormalizedUserName = "WILSON@HEALTHESTEEM.CA",
+                         Email = "Wilson@HealthEsteem.ca",
+                         NormalizedEmail = "WILSON@HEALTHESTEEM.CA",
+                         EmailConfirmed = false,
+                         PasswordHash = "AQAAAAEAACcQAAAAEE9w7IV4HKixVxUI2dL9uL7pO/b4bDlYefgEHfXY7ItrQ1Jbvia8xlxbPRDOH++Tsw==", //Abc123!
+                         SecurityStamp = "AW74NOXTMIRJLAGYJOL34XFRDUZAKCNJ",
+                         ConcurrencyStamp = "b3103776-1b4b-47e0-af12-a35245e52f4c",
+                         PhoneNumber = null,
+                         PhoneNumberConfirmed = false,
+                         TwoFactorEnabled = false,
+                         LockoutEnd = null,
+                         LockoutEnabled = true,
+                         AccessFailedCount = 0,
+                         FirstName = "Wilson",
+                         LastName = "Nie",
+                         DivisionID = 2,
+                         UnitID = 5
+                     }
+                );
             context.SaveChanges();
         }
     }
