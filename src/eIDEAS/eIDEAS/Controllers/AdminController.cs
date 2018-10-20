f351a21071823024db3037fe53f27076831e9570
Shawn <shawn.clake@gmail.com>
2018-10-19 20:21:34 -0600

    Added admin panel, initial unit/division CRUD, and fancy error pages.

---
+++ src/eIDEAS/eIDEAS/Controllers/AdminController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eIDEAS.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Homepage()
+        {
+            return View();
+        }
+
+        /*[HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Name")] Division division)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(division);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(division);
+        }*/
+    }
+}
