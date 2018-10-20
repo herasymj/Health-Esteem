219feba3883b13b26895aaf7cc197678b5d43760
oscar666666 <1660325367@qq.com>
2018-10-19 21:11:42 -0600

    Integration Test Authration page success

--- src/eIDEAS/eIDEAS/UnitTests/AuthTest.cs
+++ src/eIDEAS/eIDEAS/UnitTests/AuthTest.cs
@@ -7,6 +7,9 @@ using Xunit;
 using AngleSharp;
 using Microsoft.AspNetCore.Mvc.Testing;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace eIDEAS.UnitTests
 {
@@ -29,6 +32,7 @@ namespace eIDEAS.UnitTests
         //[InlineData("/Contact")]
         //[InlineData("/Register")]
         //[InlineData("/Login")]
+        
         [Fact]
         public async Task PTestAsync()
         {
@@ -45,7 +49,40 @@ namespace eIDEAS.UnitTests
                 response.Content.Headers.ContentType.ToString());
             //response.IsSuccessStatusCode.IsTrue();
         }
-        
+        [Fact]
+        public async Task Should_be_able_to_see_secret()
+        {
+            var data = new Dictionary<string, string>()
+    {
+        { "Email", "Jennifer@HealthEsteem.ca" },
+        { "Password", "Abc123!" }
+    };
+            string url = "/identity/Account/Login";
+            var client = _factory.CreateClient();
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            var secretContent = new FormUrlEncodedContent(data);
+            try
+            {
+                string xx = JsonConvert.SerializeObject(secretContent);
+                //JsonSerializer.Serialize( jsonWriter, secretContent);
+                byte[] mm = System.Text.Encoding.UTF8.GetBytes(xx);
+                var cont = new ByteArrayContent(mm);
+                cont.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+                var result = await client.PostAsync("/identity/Account/Login", cont);
+             Assert.True(result.Headers.Location.ToString()==(""));
+            }
+            catch (Exception x)
+            {
+
+            }
+            //client.DefaultRequestHeaders.Add("my-name", "test");
+            //client.DefaultRequestHeaders.Add("my-id", "12345");
             
+           
         }
+
+    }
     }
