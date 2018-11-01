866de1cf99799e699788b4d58a391f1250473f75
QuinnBast <Quinn.Bast@hotmail.com>
2018-11-01 15:25:22 -0600

    Created leaderboard

---
+++ src/eIDEAS/eIDEAS/Controllers/LeaderboardController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eIDEAS.Data;
+using eIDEAS.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eIDEAS.Controllers
+{
+    public class LeaderboardController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public LeaderboardController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            var leaderboardModel = _context.Users.OrderByDescending(user => user.IdeaPoints).ToList();
+            return View(leaderboardModel);
+        }
+    }
+}
