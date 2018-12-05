3f58f5f3c3d7b7b8f3ca196ffa47a238abff9a66
herasymj <jennifer.herasymuik@gmail.com>
2018-12-04 22:18:10 -0600

    Unit tests added for new controllers.

--- src/eIDEAS/eIDEAS/UnitTests/HomeControllerTest.cs
+++ src/eIDEAS/eIDEAS/UnitTests/HomeControllerTest.cs
@@ -1,49 +1,68 @@
-﻿//using Microsoft.AspNetCore.Mvc;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
-//using Xunit;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eIDEAS.Data;
+using Xunit;
+using Microsoft.EntityFrameworkCore;
+using eIDEAS.Models;
+using eIDEAS.Areas.Identity.Pages.Account;
+using Microsoft.AspNetCore.Identity;
+using Moq;
 
-//namespace eIDEAS.UnitTests
-//{
-//    public class HomeControllerTest
-//    {
-//        private eIDEAS.Controllers.HomeController controller = new Controllers.HomeController();
+namespace eIDEAS.UnitTests
+{
+    public class HomeControllerTest
+    {
+        ApplicationDbContext _context;
+        private eIDEAS.Controllers.HomeController controller;
 
-//        [Fact]
-//        public void LoginPageExists()
-//        {
-//            ViewResult indexPage = controller.Index() as ViewResult;
-//            Assert.NotNull(indexPage);
-//        }
+        public HomeControllerTest()
+        {
+            //Setup the database context in the testing envorinment
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseInMemoryDatabase();
+            this._context = new ApplicationDbContext(optionsBuilder.Options);
+            var userStoreMock = new Mock<IUserStore<ApplicationUser>>();
+            var userManager = new Mock<UserManager<ApplicationUser>>(userStoreMock.Object, null, null, null, null, null, null, null, null);
 
-//        [Fact]
-//        public void ContactPageExists()
-//        {
-//            ViewResult contactPage = controller.Contact() as ViewResult;
-//            Assert.NotNull(contactPage);
-//        }
+            this.controller = new Controllers.HomeController(_context, userManager.Object);
+        }
 
-//        [Fact]
-//        public void FAQPageExists()
-//        {
-//            ViewResult FAQPage = controller.FAQ() as ViewResult;
-//            Assert.NotNull(FAQPage);
-//        }
+        [Fact]
+        public void LoginPageExists()
+        {
+            ViewResult indexPage = controller.Index() as ViewResult;
+            Assert.NotNull(indexPage);
+        }
 
-//        [Fact]
-//        public void ErrorPageExists()
-//        {
-//            ViewResult aboutPage = controller.Error(404) as ViewResult;
-//            Assert.NotNull(aboutPage);
-//        }
+        [Fact]
+        public void ContactPageExists()
+        {
+            ViewResult contactPage = controller.Contact() as ViewResult;
+            Assert.NotNull(contactPage);
+        }
 
-//        [Fact]
-//        public void PrivacyPageExists()
-//        {
-//            ViewResult aboutPage = controller.Privacy() as ViewResult;
-//            Assert.NotNull(aboutPage);
-//        }
-//    }
-//}
+        [Fact]
+        public void FAQPageExists()
+        {
+            ViewResult FAQPage = controller.FAQ() as ViewResult;
+            Assert.NotNull(FAQPage);
+        }
+
+        [Fact]
+        public void ErrorPageExists()
+        {
+            ViewResult aboutPage = controller.Error(404) as ViewResult;
+            Assert.NotNull(aboutPage);
+        }
+
+        [Fact]
+        public void PrivacyPageExists()
+        {
+            ViewResult aboutPage = controller.Privacy() as ViewResult;
+            Assert.NotNull(aboutPage);
+        }
+    }
+}
